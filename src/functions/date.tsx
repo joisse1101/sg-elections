fde431704a321168892674c06488203a8cab7426
joisse1101 <thoiwei@gmail.com>
2024-10-02 14:55:05 +0800

    Helper functions to calc time diff

---
+++ src/functions/date.tsx
@@ -0,0 +1,27 @@
+let day_ms = 24 * 60 * 60 * 1000
+let month_ms = 30.437 * day_ms
+let year_ms = 365.25 * day_ms
+
+function timeDiff_ms(date_now: Date, date_past: Date) {
+    return date_now.valueOf() - date_past.valueOf()
+}
+
+function year_diff(date_now: Date, date_past: Date) {
+    return Math.floor(timeDiff_ms(date_now, date_past) / year_ms)
+}
+function month_diff(date_now: Date, date_past: Date) {
+    return Math.floor(timeDiff_ms(date_now, date_past) / month_ms)
+}
+function day_diff(date_now: Date, date_past: Date) {
+    return Math.floor(timeDiff_ms(date_now, date_past) / day_ms)
+}
+
+export function timeDiff_ymd(date_now: Date, date_past: Date) {
+    let years = year_diff(date_now, date_past)
+    date_past.setFullYear(date_past.getFullYear() + years)
+    let months = month_diff(date_now, date_past)
+    date_past.setMonth(date_past.getMonth() + months)
+    let days = day_diff(date_now, date_past)
+
+    return new Set<number>([years, months, days])
+}
